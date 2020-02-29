9ddc4c7c7e60fd4c0f227216b1422b611438fda4
Rafael Delboni <rafadelboni@gmail.com>
2020-02-29 10:21:20 -0300

    feat: Using a centralized keyboardswap script

--- config/i3/keyboardswap.sh
+++ config/i3/keyboardswap.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
+uk_layout_cmd="setxkbmap -layout gb -variant extd -option caps:swapescape"
+us_layout_cmd="setxkbmap -layout us -variant intl -option caps:swapescape"
+
 #switch between two layouts (Us and Uk in this case)
 current_layouts=$(setxkbmap -query | grep layout)
 
-if [ "${current_layouts: -2}" = "us" ]; then
-  setxkbmap -layout gb -variant extd -option caps:swapescape
-else
-  setxkbmap -layout us -variant intl -option caps:swapescape
+#no argument swap between us - uk
+if [ -z $1 ]; then
+  if [ "${current_layouts: -2}" = "us" ]; then
+    eval $uk_layout_cmd
+  else
+    eval $us_layout_cmd
+  fi
+#us argument
+elif [ $1 == 'us' ]; then
+  eval $us_layout_cmd
+#uk argument
+elif [ $1 == 'uk' ]; then
+  eval $uk_layout_cmd
 fi
 
 xset r rate 150 55
