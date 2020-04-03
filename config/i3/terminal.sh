09eb1bf2df52a4d748f3a38483830144cc27cc94
Rafael Delboni <rafadelboni@gmail.com>
2020-04-03 16:42:35 -0300

    fix: Change var alacritty 0.4.2 uses WINIT_X11_SCALE_FACTOR

--- config/i3/terminal.sh
+++ config/i3/terminal.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-[[ -z "${LOCAL_DPI_FACTOR}" ]] && WINIT_HIDPI_FACTOR=1.4 || WINIT_HIDPI_FACTOR="${LOCAL_DPI_FACTOR}"
-WINIT_HIDPI_FACTOR=$WINIT_HIDPI_FACTOR alacritty -e tmux new-session
+WINIT_X11_SCALE_FACTOR=${LOCAL_DPI_FACTOR:-1.4} alacritty -e tmux new-session
