337e7b809ca09014623c653f2c45b9dc09cf3f29
Rafael Delboni <rafadelboni@gmail.com>
2020-04-21 10:55:15 -0300

    feat: Changes scrot by maim

--- config/i3/lock.sh
+++ config/i3/lock.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-scrot -o /tmp/screenshot.png
-convert -scale 10% -blur 0x0.5 -resize 1000% /tmp/screenshot.png /tmp/screenshot.png
-i3lock -i /tmp/screenshot.png
+i3lock -i $HOME/.config/i3/josh-rose-trYl7JYATH0-unsplash.png
