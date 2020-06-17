acbb0ab6d5c1f68260446b16af0aac5a91537926
Rafael Delboni <rafadelboni@gmail.com>
2020-06-17 14:46:55 -0300

    fix: Updates ncider

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-ancient "0.6.15"]]}
- :repl {:plugins [[cider/cider-nrepl "0.24.0"]]
+ :repl {:plugins [[cider/cider-nrepl "0.25.2"]]
         :repl-options {:timeout 120000}}}
