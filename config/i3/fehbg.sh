403200aeeb8e8236554722d27c042362754bb0c6
Rafael Delboni <rafadelboni@gmail.com>
2021-06-18 10:13:59 -0300

    feat: New wallpaper and printscreen

--- config/i3/fehbg.sh
+++ config/i3/fehbg.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-feh --bg-scale $HOME/.dotfiles/config/i3/bernard-hermant-p6x8goM73Tg-unsplash.jpg
+feh --bg-scale $HOME/.dotfiles/config/i3/tianshu-liu-SBK40fdKbAg-unsplash.jpg
