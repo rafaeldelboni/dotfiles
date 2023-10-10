234a021a12a0ad91d99fdb1f27099d985f9aee6e
Rafael Delboni <rafadelboni@gmail.com>
2023-10-09 21:23:21 -0300

    feat: adds sexp surround cmds

--- config/nvim/fnl/plugins/paredit.fnl
+++ config/nvim/fnl/plugins/paredit.fnl
@@ -6,7 +6,34 @@
   :ft [:clojure :fennel]
   :config (fn []
             (let [paredit (require :nvim-paredit)]
-              (paredit.setup)))}
+              (paredit.setup
+                {:keys {:<localleader>I [(fn []
+                                           (paredit.cursor.place_cursor
+                                             (paredit.wrap.wrap_enclosing_form_under_cursor "(" ")")
+                                             {:mode :insert
+                                              :placement :inner_end}))
+                                         "Wrap form insert tail"]
+
+                        :<localleader>W [(fn []
+                                           (paredit.cursor.place_cursor
+                                             (paredit.wrap.wrap_element_under_cursor "(" ")")
+                                             {:mode :insert
+                                              :placement :inner_end}))
+                                         "Wrap element insert tail"]
+
+                        :<localleader>i [(fn []
+                                           (paredit.cursor.place_cursor
+                                             (paredit.wrap.wrap_enclosing_form_under_cursor "( " ")")
+                                             {:mode :insert
+                                              :placement :inner_start}))
+                                         "Wrap form insert head"]
+
+                        :<localleader>w [(fn []
+                                           (paredit.cursor.place_cursor
+                                             (paredit.wrap.wrap_element_under_cursor "( " ")")
+                                             {:mode :insert
+                                              :placement :inner_start}))
+                                         "Wrap element insert head"]}})))}
 
  {1 :julienvincent/nvim-paredit-fennel
   :dependencies [:julienvincent/nvim-paredit]
--- config/nvim/lua/plugins/paredit.lua
+++ config/nvim/lua/plugins/paredit.lua
@@ -4,14 +4,26 @@ local autoload = _local_1_["autoload"]
 local nvim = autoload("nvim")
 local function _2_()
   local paredit = require("nvim-paredit")
-  return paredit.setup()
+  local function _3_()
+    return paredit.cursor.place_cursor(paredit.wrap.wrap_enclosing_form_under_cursor("(", ")"), {mode = "insert", placement = "inner_end"})
+  end
+  local function _4_()
+    return paredit.cursor.place_cursor(paredit.wrap.wrap_element_under_cursor("(", ")"), {mode = "insert", placement = "inner_end"})
+  end
+  local function _5_()
+    return paredit.cursor.place_cursor(paredit.wrap.wrap_enclosing_form_under_cursor("( ", ")"), {mode = "insert", placement = "inner_start"})
+  end
+  local function _6_()
+    return paredit.cursor.place_cursor(paredit.wrap.wrap_element_under_cursor("( ", ")"), {mode = "insert", placement = "inner_start"})
+  end
+  return paredit.setup({keys = {["<localleader>I"] = {_3_, "Wrap form insert tail"}, ["<localleader>W"] = {_4_, "Wrap element insert tail"}, ["<localleader>i"] = {_5_, "Wrap form insert head"}, ["<localleader>w"] = {_6_, "Wrap element insert head"}}})
 end
-local function _3_()
+local function _7_()
   local paredit_fnl = require("nvim-paredit-fennel")
   return paredit_fnl.setup()
 end
-local function _4_()
+local function _8_()
   local surround = require("nvim-surround")
   return surround.setup()
 end
-return {{"julienvincent/nvim-paredit", lazy = true, ft = {"clojure", "fennel"}, config = _2_}, {"julienvincent/nvim-paredit-fennel", dependencies = {"julienvincent/nvim-paredit"}, lazy = true, ft = {"fennel"}, config = _3_}, {"kylechui/nvim-surround", event = "VeryLazy", config = _4_}, {"windwp/nvim-autopairs", event = "InsertEnter", opts = {}}}
+return {{"julienvincent/nvim-paredit", lazy = true, ft = {"clojure", "fennel"}, config = _2_}, {"julienvincent/nvim-paredit-fennel", dependencies = {"julienvincent/nvim-paredit"}, lazy = true, ft = {"fennel"}, config = _7_}, {"kylechui/nvim-surround", event = "VeryLazy", config = _8_}, {"windwp/nvim-autopairs", event = "InsertEnter", opts = {}}}
