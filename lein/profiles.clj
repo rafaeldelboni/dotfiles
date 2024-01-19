dc9b11a598a7030896e4b227fdb65cb50c1b5adc
Rafael Delboni <rafadelboni@gmail.com>
2024-01-19 12:56:02 -0300

    chore: Update profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,21 +1,21 @@
 {:user
- {:min-lein-version    "2.9.0"
+ {:min-lein-version    "2.10.0"
   :repositories        [["nu-codeartifact" {:url "https://maven.cicd.nubank.world"}]]
   :plugin-repositories [["nu-codeartifact" {:url "https://maven.cicd.nubank.world"}]]
-  :jvm-opts            ["-Djava.locale.providers=COMPAT,JRE,CLDR"
-                        "-Dclojure.core.async.go-checking=true" ;; Enables strict checks of blocking calls inside core.async `go` context
-                        "--illegal-access=deny"]            ;; Note: this option was removed in Java 17
+  :jvm-opts            [;; Uses locale compatibility mode, because existing code expects JDK8
+                        ;; behavior when formatting time and money
+                        "-Djava.locale.providers=COMPAT,CLDR"
+                        ;; Enables strict checks of blocking calls inside core.async `go` context
+                        "-Dclojure.core.async.go-checking=true"]
   :plugins             [[com.jakemccrary/lein-test-refresh "0.25.0"]
-                        [lein-ancient "1.0.0-RC3"]]}
+                        [lein-ancient "1.0.0-RC3"]]
 
- :test {:dependencies [[cljdev "0.11.0"]]
-        :injections   [(require 'nu)]}
+  :dependencies [[cljdev "0.11.8"]]
+  :injections   [(require 'nu)]}
 
  :repl {;; If you need to have `cider-nrepl` and `refactor-nrepl` in your REPL
         ;; session (i.e.: vim-users), uncomment the following lines
-        ;; :plugins [[cider/cider-nrepl "0.25.3"]
+        ;; :plugins [[cider/cider-nrepl "0.30.0"]
         ;;           [refactor-nrepl "2.5.0-SNAPSHOT"]]
-        :plugins      [[cider/cider-nrepl "0.28.7"]]
-        :repl-options {:timeout 300000}
-        :dependencies [[cljdev "0.11.0"]]
-        :injections   [(require 'nu)]}}
+        :plugins      [[cider/cider-nrepl "0.45.0"]]
+        :repl-options {:timeout 300000}}}
