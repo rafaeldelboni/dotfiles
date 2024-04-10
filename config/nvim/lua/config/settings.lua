8f997a2a72486abb1986016b2b92f61e4d449208
Rafael Delboni <rafadelboni@gmail.com>
2024-04-10 08:51:33 -0300

    bump: lazy & clj deps

--- config/nvim/lua/config/settings.lua
+++ config/nvim/lua/config/settings.lua
@@ -10,7 +10,7 @@ nvim.ex.autocmd("FileType", "cs", "setlocal tabstop=4 softtabstop=4 shiftwidth=4
 nvim.ex.autocmd("FileType", "gdscript,gdshader", "setlocal foldmethod=expr tabstop=4 shiftwidth=4 indentexpr= noexpandtab")
 nvim.ex.set("nowrap")
 do
-  local options = {encoding = "utf-8", spelllang = "en_us", backspace = "2", colorcolumn = "80", errorbells = true, number = true, ruler = true, completeopt = "menuone,noselect", wildmenu = true, wildignore = "*/tmp/*,*.so,*.swp,*.zip", ignorecase = true, smartcase = true, clipboard = "unnamedplus", list = true, listchars = str.join(",", {"tab:\226\150\182-", "trail:\226\128\162", "extends:\194\187", "precedes:\194\171", "eol:\194\172"}), expandtab = true, tabstop = 2, shiftwidth = 2, softtabstop = 2, undofile = true, splitbelow = true, splitright = true, hlsearch = true, signcolumn = "number", swapfile = false, backup = false, showmode = false}
+  local options = {encoding = "utf-8", spelllang = "en_us", backspace = "2", colorcolumn = "80", errorbells = true, number = true, ruler = true, completeopt = "menuone,noselect", wildmenu = true, wildignore = "*/tmp/*,*.so,*.swp,*.zip", ignorecase = true, smartcase = true, clipboard = "unnamedplus", list = true, listchars = str.join(",", {"tab:\226\150\182-", "trail:\226\128\162", "extends:\194\187", "precedes:\194\171", "eol:\194\172"}), expandtab = true, tabstop = 2, shiftwidth = 2, softtabstop = 2, undofile = true, splitbelow = true, splitright = true, hlsearch = true, signcolumn = "number", backup = false, showmode = false, swapfile = false}
   for option, value in pairs(options) do
     core.assoc(nvim.o, option, value)
   end
